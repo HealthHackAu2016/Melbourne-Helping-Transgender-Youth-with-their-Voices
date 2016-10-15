f7d324b147c6f2e480b379f45b1cb60546e1d6b2
Lexa Francis <oscardenley@gmail.com>
2016-10-15 14:43:52 +1100

    Added Unity project

---
+++ TransVoicesUnityProject/Assets/ArcheryAnimation.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ArcheryAnimation: MonoBehaviour {
+
+   public Transform   arrow, target, spawnPoint;
+   public GameObject  hitDecal;
+   public float       arcDuration, arcHeight, targetRadius;
+   public int         maxHitDecals;
+   public GameManager gameManager;
+
+   List< GameObject > hitDecalInstances = new List< GameObject >();
+   
+   public bool animating { get; private set; }
+
+   public void Fire( float score ) {
+
+      StartCoroutine( AnimationRoutine( score ) );
+   }
+
+   IEnumerator AnimationRoutine( float score ) {
+
+      animating       = true;
+      float startTime = Time.time;
+      gameManager.startButton.interactable = false;
+
+      float   normalizedScore = Mathf.InverseLerp( gameManager.baselineScore, gameManager.goalScore, score );
+      float   hitRadius       = Mathf.Lerp( targetRadius, 0.0f, normalizedScore );
+      Vector3 hitPoint        = target.position + (Vector3) (Random.insideUnitCircle.normalized * hitRadius);
+
+      while (Time.time < startTime + arcDuration) {
+
+         float t        = (Time.time - startTime) / arcDuration;
+         float extraY   = (1.0f - Mathf.Pow( t * 2.0f - 1.0f, 2.0f )) * arcHeight;
+         arrow.position = Vector3.Lerp( spawnPoint.position, hitPoint, t ) + Vector3.up * extraY;
+         
+         yield return null;
+      }
+      Vector3    decalPoint    = hitPoint + Vector3.back * 0.05f;
+      GameObject decalInstance = Instantiate( hitDecal, decalPoint, Quaternion.identity ) as GameObject;
+      hitDecalInstances.Add( decalInstance );
+
+      if (hitDecalInstances.Count > maxHitDecals) {
+
+         Destroy( hitDecalInstances[ 0 ] );
+         hitDecalInstances.RemoveAt( 0 );
+      }
+      arrow.position = hitPoint;
+      animating      = false;
+      gameManager.startButton.interactable = true;
+   }
+}
---
+++ TransVoicesUnityProject/Assets/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GameManager: MonoBehaviour {
+
+   public float            baselineScore = -1.0f, goalScore = -1.0f;
+   public Text             baselineScoreNumber, goalScoreNumber,
+                           lastScoreText, turnsText, highestScoreText, averageScoreText;
+   public Button           startButton;
+   public ArcheryAnimation archeryAnimation;
+
+   List< float > scores = new List< float >();
+   float         highestScore;
+   
+   public void SetBaselineScore( float score ) {
+
+      baselineScore            = score;
+      baselineScoreNumber.text = score.ToString("0.00");
+   }
+   
+   public void SetGoalScore( float score ) {
+
+      goalScore            = score;
+      goalScoreNumber.text = score.ToString("0.00");
+   }
+
+   public void StartTurn() {
+      
+   }
+
+   public void GotTurnResult( float resultScore ) {
+
+      archeryAnimation.Fire( resultScore );
+      scores.Add( resultScore );
+
+      lastScoreText.text    = resultScore.ToString("0.00");
+      turnsText.text        = scores.Count.ToString();
+      averageScoreText.text = scores.Average().ToString("0.00");
+
+      if (resultScore > highestScore) {
+
+         highestScore          = resultScore;
+         highestScoreText.text = resultScore.ToString("0.00");
+      }
+   }
+
+   public void TestTurn() {
+
+      float score = Random.Range( baselineScore, goalScore );
+      GotTurnResult( score );
+   }
+}
